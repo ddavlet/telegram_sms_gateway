9b918559b866239e6d2bf0420f127b46fb1f6db1
Dilshod Davletyarov <dilshod95@gmail.com>
2024-06-23 00:12:53 +0500

    webhook setup in prod mode

--- main.py
+++ main.py
@@ -49,6 +49,19 @@ app = FastAPI()
 # except Exception as e:
 #     print(f"Error on setting up webhook: {e}")
 
+# Run mode get my ip
+
+async def get_my_ip() -> None:
+    try:
+        response = await client.get('https://api.ipify.org')
+        print("IP retrieved successfully")
+        response = await client.get(f'{SETWEBHOOK_URL}?url={response.text}/webhook')
+        print(f"Status Code: {response.status_code}")
+        print(f"Response Text: {response.text}")
+    except Exception as e:
+        print("Couldn't retrieve ip")
+
+get_my_ip()
 
 # middleware restrictions (safety)
 allowed_ips = [
